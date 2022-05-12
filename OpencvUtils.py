1178d19c6458ed0960f5169acd7ad1d373bfd27e
Ettore Candeloro <candeloroettore@gmail.com>
2022-05-12 14:52:14 +0200

    updated code

--- OpencvUtils.py
+++ OpencvUtils.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+# OPENCV
+
 
 def resize(frame, scale_percent):
     """
@@ -18,6 +20,96 @@ def resize(frame, scale_percent):
     resized = cv2.resize(frame, dim, interpolation=cv2.INTER_LINEAR)
     return resized
 
+
+# EULER ANGLES AND ROTATIONS MATRIX
+
+def isRotationMatrix(R, precision=1e-4):
+    """
+    Checks if a matrix is a rotation matrix
+    :param R: np.array matrix of 3 by 3
+    :param precision: float
+        precision to respect to accept a zero value in identity matrix check (default is 1e-4)
+    :return: True or False
+        Return True if a matrix is a rotation matrix, False if not
+    """
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < precision
+
+
+def eulerAnglesToRotationMatrix(theta):
+    '''
+    :param: theta: np array 
+        array of angles (radians) in the order of YAW(psi), PITCH(theta), ROLL(phi)
+
+    YAW = rotation around the x axis
+    PITCH = rotation around the y axis
+    ROLL = rotation around the z axis
+
+    :return: 3 by 3 np array
+        Return 3x3 rotation matrix
+    '''
+    # yaw rotation
+    R_x = np.array([[1,         0,                  0],
+                    [0,         np.cos(theta[0]), -np.sin(theta[0])],
+                    [0,         np.sin(theta[0]), np.cos(theta[0])]
+                    ])
+    # pitch rotation
+    R_y = np.array([[np.cos(theta[1]),    0,      np.sin(theta[1])],
+                    [0,                     1,      0],
+                    [-np.sin(theta[1]),   0,      np.cos(theta[1])]
+                    ])
+    # roll rotation
+    R_z = np.array([[np.cos(theta[2]),    -np.sin(theta[2]),    0],
+                    [np.sin(theta[2]),    np.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+
+    R = np.dot(R_z, np.dot(R_y, R_x))
+    # the dot product here is with pre-multiplication, so Rz*Ry*Rx
+    # this is because we are considering the same reference systems for all the 3 rotations!
+
+    return R
+
+
+def rotationMatrixToEulerAngles(R, precision=1e-4):
+    '''
+    Computes the Tait–Bryan Euler (XYZ) angles from a Rotation Matrix.
+    Also checks if there is a gymbal lock and eventually use an alternative formula
+    :param R: np.array
+        3 x 3 Rotation matrix
+    :param precision: float
+        precision to respect to accept a zero value in identity matrix check (default is 1e-4)
+    :return: (yaw, pitch, roll) tuple of float numbers
+        Euler angles in radians in the order of YAW, PITCH, ROLL
+    '''
+
+    # Calculates Tait–Bryan Euler angles from a Rotation Matrix
+    assert (isRotationMatrix(R, precision))  # check if it's a Rmat
+
+    # assert that sqrt(R11^2 + R21^2) != 0
+    sy = np.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+    singular = sy < precision
+
+    if not singular:  # if not in a singularity, use the standard formula
+        x = np.arctan2(R[2, 1], R[2, 2])  # atan2(R31, R33) -> YAW, angle PSI
+
+        # atan2(-R31, sqrt(R11^2 + R21^2)) -> PITCH, angle delta
+        y = np.arctan2(-R[2, 0], sy)
+
+        z = np.arctan2(R[1, 0], R[0, 0])  # atan2(R21,R11) -> ROLL, angle phi
+
+    else:  # if in gymbal lock, use different formula for yaw, pitch roll
+        x = np.arctan2(-R[1, 2], R[1, 1])
+        y = np.arctan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])  # returns YAW, PITCH, ROLL angles in radians
+
+
+# DLIB
 
 def get_face_area(face):
     """
@@ -72,88 +164,3 @@ def get_array_keypoints(landmarks, dtype="int", verbose: bool = False):
         print(points_array)
 
     return points_array
-
-
-def isRotationMatrix(R, precision=1e-4):
-    """
-    Checks if a matrix is a rotation matrix
-    :param R: np.array matrix of 3 by 3
-    :param precision: float
-        precision to respect to accept a zero value in identity matrix check (default is 1e-4)
-    :return: True or False
-        Return True if a matrix is a rotation matrix, False if not
-    """
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype=R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < precision
-
-# Calculates Rotation Matrix given euler angles in the order of YAW(psi), PITCH(theta), ROLL(phi)
-
-
-def eulerAnglesToRotationMatrix(theta):
-    '''
-    :param: theta: np array 
-        array of angles (radians) in the order of YAW(psi), PITCH(theta), ROLL(phi)
-
-    YAW = rotation around the x axis
-    PITCH = rotation around the y axis
-    ROLL = rotation around the z axis
-
-    :return: 3 by 3 np array
-        Return 3x3 rotation matrix
-    '''
-    # yaw rotation
-    R_x = np.array([[1,         0,                  0],
-                    [0,         np.cos(theta[0]), -np.sin(theta[0])],
-                    [0,         np.sin(theta[0]), np.cos(theta[0])]
-                    ])
-    # pitch rotation
-    R_y = np.array([[np.cos(theta[1]),    0,      np.sin(theta[1])],
-                    [0,                     1,      0],
-                    [-np.sin(theta[1]),   0,      np.cos(theta[1])]
-                    ])
-    # roll rotation
-    R_z = np.array([[np.cos(theta[2]),    -np.sin(theta[2]),    0],
-                    [np.sin(theta[2]),    np.cos(theta[2]),     0],
-                    [0,                     0,                      1]
-                    ])
-
-    R = np.dot(R_z, np.dot(R_y, R_x))
-
-    return R
-
-
-def rotationMatrixToEulerAngles(R, precision=1e-4):
-    """
-    Computes the Tait–Bryan Euler () angles from a Rotation Matrix.
-    Also checks if there is a gymbal lock and eventually use an alternative formula
-    :param R: np.array
-        3 x 3 Rotation matrix
-    :param precision: float
-        precision to respect to accept a zero value in identity matrix check (default is 1e-4)
-    :return: (yaw, pitch, roll) tuple of float numbers
-        Euler angles in radians in the order of YAW, PITCH, ROLL
-    """
-    # Calculates Tait–Bryan Euler angles from a Rotation Matrix
-    assert (isRotationMatrix(R, precision))  # check if it's a Rmat
-
-    # assert that sqrt(R11^2 + R21^2) != 0
-    sy = np.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-    singular = sy < precision
-
-    if not singular:  # if not in a singularity, use the standard formula
-        x = np.arctan2(R[2, 1], R[2, 2])  # atan2(R31, R33) -> YAW, angle PSI
-
-        # atan2(-R31, sqrt(R11^2 + R21^2)) -> PITCH, angle delta
-        y = np.arctan2(-R[2, 0], sy)
-
-        z = np.arctan2(R[1, 0], R[0, 0])  # atan2(R21,R11) -> ROLL, angle phi
-
-    else:  # if in gymbal lock, use different formula for yaw, pitch roll
-        x = np.arctan2(-R[1, 2], R[1, 1])
-        y = np.arctan2(-R[2, 0], sy)
-        z = 0
-
-    return np.array([x, y, z])  # returns YAW, PITCH, ROLL in radians
